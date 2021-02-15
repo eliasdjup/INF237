77de05e70797aa9aa2307137d5f2ac530ed6e362
PålHH <Paal_hannus@hotmail.com>
2021-02-15 12:35:14 +0100

    Reversing roads: Done

---
+++ Graphs1/RevRoads.py
@@ -0,0 +1,80 @@
+
+from sys import stdin
+
+iterator = iter(stdin.readlines())
+history = []
+case = 0
+
+while True:
+    try:
+        local_history = []
+        m, n = map(int, next(iterator).split())
+        local_history.append((m,n))
+
+        for inp in range(n):
+            a,b =  map(int, next(iterator).split())
+            local_history.append((a,b))
+        
+        history.append(local_history)
+        case += 1
+
+    except StopIteration:
+        break
+
+def dfs(current_node, visited, graph):
+    stack = [current_node]
+    while len(stack) != 0:
+        current = stack.pop()
+
+        if current in visited:
+            continue
+
+        visited.append(current)
+
+        for neighbour in graph[current]:
+            if neighbour not in visited:
+                stack.append(neighbour)
+
+    return visited
+
+def checkValidity(graph, locations):
+        all_routes = [dfs(x, list(), graph) for x in range(locations)]
+        success = True
+        for route in all_routes:
+            if len(route) != locations: # Are all locations reachable?
+                success = False
+        return success
+
+def toGraph(graph_list):
+    graph = {}
+    m,n = graph_list[0]
+    for i in range(m):
+        graph[i] = []
+    for (a,b) in graph_list[1:]:
+        lst = graph[a]
+        lst.append(b)
+        graph[a] = lst
+    return graph
+
+def calc(history):
+    for casenum, case_history in enumerate(history):
+
+        case_graph = toGraph(case_history)
+        valid = checkValidity(case_graph, case_history[0][0])
+        if valid:
+            print(f"Case {casenum + 1}: valid")
+        else:
+            m,n = case_history[0]
+
+            for idx, (a,b) in enumerate(case_history[1:]):
+                case_history[idx+1] = (b,a)
+                valid = checkValidity(toGraph(case_history), m)
+                if valid:
+                    print(f"Case {casenum + 1}: {a} {b}")
+                    break
+                case_history[idx+1] = (a,b)
+        
+            if not valid:
+                print(f"Case {casenum + 1}: Invalid")
+
+calc(history)
--- Graphs1/Reversing_Roads.py
+++ Graphs1/Reversing_Roads.py
@@ -5,7 +5,7 @@ import itertools
 
 def dfs(current_node, visited, graph):
     if current_node not in visited:
-        visited.add(current_node)
+        visited.append(current_node)
         for neighbour in graph[current_node]:
             dfs(neighbour, visited, graph)
     return visited
@@ -14,43 +14,120 @@ def dfs(current_node, visited, graph):
 iterator = iter(stdin.readlines())
 graphs = {}
 locations_by_case = []
+history = []
 case = 0
+
+
 while True:
     try:
+        local_history = []
         m, n = map(int, next(iterator).split())
         locations_by_case.append(m)
+        local_history.append((m,n))
         graph = {}
+        for i in range(m):
+            graph[i] = []
         for inp in range(n):
             a,b =  map(int, next(iterator).split())
+            local_history.append((a,b))
+
             connections = graph.get(a, [])
             connections.append(b)
             graph[a] = connections
-
-            connections = graph.get(b, [])
-            connections.append(a)
-            graph[b] = connections
         
+        history.append(local_history)
         graphs[case] = graph
         case += 1
 
     except StopIteration:
         break
-    except EOFError: # Trengs nok ikke da iteratoren sender stopiteration exception
-        break
 
-for index, locations_in_case in enumerate(locations_by_case):
-    location_set = set()
-    for i in range(locations_in_case):
-        
-        [location_set.add(x) for x in dfs(0, location_set, graphs[index])]
-        #location_set.add(dfs(0, location_set, graphs[0]))
-        #print(location_set)
+
+def checkValidity(graph, locations):
+        all_routes = [dfs(x, list(), graph) for x in range(locations)]
+        success = True
+        for route in all_routes:
+            if len(route) != locations: # Are all locations reachable?
+                success = False
+        return success
+
+
+#print(graphs[2])
+def calc(graphs, locations_by_case):
+    for case in range(len(locations_by_case)):
+
+        graph = graphs[case]
+        locations = locations_by_case[case]
+
+        success = checkValidity(graph, locations)
+        if success:
+            print(f"Case {case+1}: valid")
+            continue
+        else:
+            m, n = history[case][0]
+            
+            valid = False
+            for idx, (a,b) in enumerate(history[case][1:]):
+                alt_history = history[case].copy()
+                alt_history[idx+1] = (b,a)
+                valid = checkValidity(toGraph(alt_history), m)
+                if valid:
+                    break
+                
+            if valid:
+                print(f"Case {case+1}: {a} {b}")
+            else:
+                print(f"Case {case+1}: invalid")
+
     
-    #if any in locations_by_case not visited...
-    #invalid
+def toGraph(graph_list):
+    graph = {}
+    m,n = graph_list[0]
+    for i in range(m):
+        graph[i] = []
+    for (a,b) in graph_list[1:]:
+        lst = graph[a]
+        lst.append(b)
+        graph[a] = lst
+    return graph
+        
+
+calc(graphs, locations_by_case)
+        
 
 
-print(graphs[0])
 
-print(locations_by_case)
-print(dfs(0, set(), graphs[0]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for x in range(locations_in_case):
+    #routes.append(dfs(x,set(),graph))
+    #dfs(x,set(),graph)
+    #print()
+    #location_set.add(dfs(0, location_set, graphs[0]))
+    #print(location_set)
+
+#if any in locations_by_case not visited...
+#invalid
+
+
+#print(graphs[0])
+
+#print(locations_by_case)
+#print(dfs(0, set(), graphs[0]))
