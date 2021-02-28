898a51008a429699128f192ed41dbbe228718f85
PålHH <Paal_hannus@hotmail.com>
2021-02-28 13:47:06 +0100

    crusaders, cardtrading

--- Sliding_Searching/CardTrading.py
+++ Sliding_Searching/CardTrading.py
@@ -1,6 +1,76 @@
 
+
 N, T, K = map(int,input().split())
-deck_list = map(int,input().split())
+
+deck={}
+prices=[]
+
+tmp = map(int, input().split())
+
+for card in tmp:
+    count = deck.get(card,0)
+    count += 1
+    deck[card] = count
+
+for i in range(T):
+    buy_price, sell_price = map(int, input().split())
+    n = deck.get(i+1,0)
+    buy = (2-n) * buy_price
+    sell = n * sell_price
+    prices.append((buy+sell, buy, sell))
+
+prices.sort(key = lambda x: x[0])
+
+cost = 0
+for i in range(K):
+    cost-= prices[i][1]
+
+for i in range(K,T):
+    cost+= prices[i][2]
+
+print(cost)
+
+
+
+'''
+i = [int(x) for x in input().split()]
+N = i[0]
+T = i[1]
+K = i[2]
+
+n_cards={}
+cardvalues=[]
+
+for c in input().split():
+    x = int(c)
+    if x in n_cards:
+        n_cards[x] += 1
+    else:
+        n_cards[x] = 1
+
+for i in range(0,T):
+    line = [int(x) for x in input().split()]
+    n = n_cards.get(i+1,0)
+    buy = (2-n)*line[0]
+    sell = n*line[1]
+    cardvalues.append([buy+sell,buy,sell])
+
+cardvalues.sort(key = lambda x: x[0])
+
+total=0
+for i in range(0,K):
+    total-= cardvalues[i][1]
+for i in range(K,T):
+    total+= cardvalues[i][2]
+
+print(total)
+
+
+
+N, T, K = map(int,input().split())
+#deck_list = map(int,input().split())
+
+
 
 buy_sell = []
 for i in range(T):
@@ -33,3 +103,5 @@ for i, e in deck.items():
 
 #print(potential_per_transaction.sort(lambda x: x[2]))
 
+
+'''
--- Sliding_Searching/crusaders.py
+++ Sliding_Searching/crusaders.py
@@ -1,3 +1,4 @@
+'''
 cities, applications = map(int, input().split())
 income = list(map(int, input().split()))
 
@@ -6,28 +7,64 @@ visited = [[]]*cities
 
 def binary_search(k, data = visited):
     lo = 0
-    hi = len(data)
+    hi = cities-1
+    returnval = 0
 
-    #print(data)
-
-    while hi - lo > 1:
-        #print(data)
+    while hi - lo >= 0:
         x = (lo + hi) // 2
 
-        if data[x] == []:
+        if visited[x] is None:
             print(f"Q {x+1}", flush=True)
-            data[x] = int(input())
+            visited[x] = int(input())
 
-        if k == data[x]:
+        if k == visited[x]:
             return x
-        elif k > data[x]:
-            lo = x
+        elif k > visited[x]:
+            if visited[x] > returnval:
+                returnval = x
+            lo = x + 1
         else:
-            hi = x
-    return lo
+            hi = x - 1
+    return returnval
 
 output = "A"
 for i in income:
     output += " " + str(binary_search(i, visited)+1)
+
+print(output)
+exit()
+'''
+cities, _ = map(int, input().split())
+income = list(map(int, input().split()))
+
+visited = [None]*cities
+visited[0] = -1
+
+
+def binary_search(k):
+    lo = 0
+    hi = cities-1
+    returnvalue = 0
+
+    while hi - lo >= 0:
+        x = (lo + hi) // 2
+
+        if visited[x] is None:
+            print(f"Q {x+1}", flush=True)
+            visited[x] = int(input())
+
+        if k == visited[x]:
+            return x
+        elif k > visited[x]:
+            if visited[x] > returnvalue:
+                returnvalue = x
+            lo = x + 1
+        else:
+            hi = x - 1
+    return returnvalue
+
+output = "A"
+for i in income:
+    output += " " + str(binary_search(i) + 1)
 
 print(output)
